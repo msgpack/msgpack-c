35fea8d6bd2d45fb0fb58bb3d6dd51e7e484b62f
Taiki Sugawara <buzz.taiki@gmail.com>
2014-08-31 22:12:47 +0900

    add msgpack_version_micro too

--- test/version.cc
+++ test/version.cc
@@ -6,8 +6,10 @@ TEST(version, print)
 	printf("MSGPACK_VERSION          : %s\n", MSGPACK_VERSION);
 	printf("MSGPACK_VERSION_MAJOR    : %d\n", MSGPACK_VERSION_MAJOR);
 	printf("MSGPACK_VERSION_MINOR    : %d\n", MSGPACK_VERSION_MINOR);
+	printf("MSGPACK_VERSION_MICRO    : %d\n", MSGPACK_VERSION_MICRO);
 	printf("msgpack_version()        : %s\n", msgpack_version());
 	printf("msgpack_version_major()  : %d\n", msgpack_version_major());
 	printf("msgpack_version_minor()  : %d\n", msgpack_version_minor());
+	printf("msgpack_version_micro()  : %d\n", msgpack_version_micro());
 }
 
