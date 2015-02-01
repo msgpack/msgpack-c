327b0db6266b9e0b86a513e93cbdd6c892d0025e
Mizuki Hirata <al11090@shibaura-it.ac.jp>
2015-02-02 02:42:41 +0900

    Added versioning namespace and test case to iterator.

--- include/msgpack/iterator.hpp
+++ include/msgpack/iterator.hpp
@@ -20,17 +20,22 @@
 #define MSGPACK_ITERATOR_HPP
 #if !defined(MSGPACK_USE_CPP03)
 
+#include <msgpack_fwd.hpp>
+
 namespace msgpack
 {
-    inline object_kv* begin(object_map &map) { return map.ptr; }
-    inline const object_kv* begin(const object_map &map) { return map.ptr; }
-    inline object_kv* end(object_map &map) { return map.ptr + map.size; }
-    inline const object_kv* end(const object_map &map) { return map.ptr + map.size; }
-    
-    inline object* begin(object_array &array) { return array.ptr; }
-    inline const object* begin(const object_array &array) { return array.ptr; }
-    inline object* end(object_array &array) { return array.ptr + array.size; }
-    inline const object* end(const object_array &array) { return array.ptr + array.size; }
+    MSGPACK_API_VERSION_NAMESPACE(MSGPACK_DEFAULT_API_NS)
+    {
+        inline object_kv* begin(object_map &map) { return map.ptr; }
+        inline const object_kv* begin(const object_map &map) { return map.ptr; }
+        inline object_kv* end(object_map &map) { return map.ptr + map.size; }
+        inline const object_kv* end(const object_map &map) { return map.ptr + map.size; }
+        
+        inline object* begin(object_array &array) { return array.ptr; }
+        inline const object* begin(const object_array &array) { return array.ptr; }
+        inline object* end(object_array &array) { return array.ptr + array.size; }
+        inline const object* end(const object_array &array) { return array.ptr + array.size; }
+    }
 }
 
 #endif // !defined(MSGPACK_USE_CPP03)
