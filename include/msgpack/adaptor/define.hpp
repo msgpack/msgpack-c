777e5f13f3629554e2db4a94c20bed315d2c6a9c
Takatoshi Kondo <redboltz@gmail.com>
2015-07-03 11:44:07 +0900

    Added map-based class pack/convert support.

--- include/msgpack/adaptor/define.hpp
+++ include/msgpack/adaptor/define.hpp
@@ -21,9 +21,19 @@
 #include "msgpack/cpp_config.hpp"
 
 #if defined(MSGPACK_USE_CPP03)
-#include "detail/cpp03_define.hpp"
+#include "detail/cpp03_define_array.hpp"
+#include "detail/cpp03_define_map.hpp"
 #else  // MSGPACK_USE_CPP03
-#include "detail/cpp11_define.hpp"
+#include "detail/cpp11_define_array.hpp"
+#include "detail/cpp11_define_map.hpp"
 #endif // MSGPACK_USE_CPP03
+
+#if defined(MSGPACK_USE_DEFINE_MAP)
+#define MSGPACK_DEFINE MSGPACK_DEFINE_MAP
+#define MSGPACK_BASE MSGPACK_BASE_MAP
+#else  // defined(MSGPACK_USE_DEFINE_MAP)
+#define MSGPACK_DEFINE MSGPACK_DEFINE_ARRAY
+#define MSGPACK_BASE MSGPACK_BASE_ARRAY
+#endif // defined(MSGPACK_USE_DEFINE_MAP)
 
 #endif // MSGPACK_DEFINE_HPP
--- include/msgpack/adaptor/detail/cpp11_define_array.hpp
+++ include/msgpack/adaptor/detail/cpp11_define_array.hpp
@@ -15,8 +15,8 @@
 //    See the License for the specific language governing permissions and
 //    limitations under the License.
 //
-#ifndef MSGPACK_CPP11_DEFINE_HPP
-#define MSGPACK_CPP11_DEFINE_HPP
+#ifndef MSGPACK_CPP11_DEFINE_ARRAY_HPP
+#define MSGPACK_CPP11_DEFINE_ARRAY_HPP
 
 #include "msgpack/versioning.hpp"
 #include "msgpack/adaptor/adaptor_base.hpp"
@@ -27,23 +27,23 @@
 #include <type_traits>
 #include <tuple>
 
-#define MSGPACK_DEFINE(...) \
+#define MSGPACK_DEFINE_ARRAY(...) \
     template <typename Packer> \
     void msgpack_pack(Packer& pk) const \
     { \
-        msgpack::type::make_define(__VA_ARGS__).msgpack_pack(pk); \
+        msgpack::type::make_define_array(__VA_ARGS__).msgpack_pack(pk); \
     } \
     void msgpack_unpack(msgpack::object const& o) \
     { \
-        msgpack::type::make_define(__VA_ARGS__).msgpack_unpack(o); \
+        msgpack::type::make_define_array(__VA_ARGS__).msgpack_unpack(o); \
     }\
     template <typename MSGPACK_OBJECT> \
     void msgpack_object(MSGPACK_OBJECT* o, msgpack::zone& z) const \
     { \
-        msgpack::type::make_define(__VA_ARGS__).msgpack_object(o, z); \
+        msgpack::type::make_define_array(__VA_ARGS__).msgpack_object(o, z); \
     }
 
-#define MSGPACK_BASE(base) (*const_cast<base *>(static_cast<base const*>(this)))
+#define MSGPACK_BASE_ARRAY(base) (*const_cast<base *>(static_cast<base const*>(this)))
 
 // MSGPACK_ADD_ENUM must be used in the global namespace.
 #define MSGPACK_ADD_ENUM(enum_name) \
@@ -95,26 +95,26 @@ MSGPACK_API_VERSION_NAMESPACE(v1) {
 namespace type {
 
 template <typename Tuple, std::size_t N>
-struct define_imp {
+struct define_array_imp {
     template <typename Packer>
     static void pack(Packer& pk, Tuple const& t) {
-        define_imp<Tuple, N-1>::pack(pk, t);
+        define_array_imp<Tuple, N-1>::pack(pk, t);
         pk.pack(std::get<N-1>(t));
     }
     static void unpack(msgpack::object const& o, Tuple& t) {
-        define_imp<Tuple, N-1>::unpack(o, t);
+        define_array_imp<Tuple, N-1>::unpack(o, t);
         const size_t size = o.via.array.size;
         if(size <= N-1) { return; }
         o.via.array.ptr[N-1].convert(std::get<N-1>(t));
     }
     static void object(msgpack::object* o, msgpack::zone& z, Tuple const& t) {
-        define_imp<Tuple, N-1>::object(o, z, t);
+        define_array_imp<Tuple, N-1>::object(o, z, t);
         o->via.array.ptr[N-1] = msgpack::object(std::get<N-1>(t), z);
     }
 };
 
 template <typename Tuple>
-struct define_imp<Tuple, 1> {
+struct define_array_imp<Tuple, 1> {
     template <typename Packer>
     static void pack(Packer& pk, Tuple const& t) {
         pk.pack(std::get<0>(t));
@@ -130,23 +130,23 @@ struct define_imp<Tuple, 1> {
 };
 
 template <typename... Args>
-struct define {
-    typedef define<Args...> value_type;
+struct define_array {
+    typedef define_array<Args...> value_type;
     typedef std::tuple<Args...> tuple_type;
-    define(Args&... args) :
+    define_array(Args&... args) :
         a(args...) {}
     template <typename Packer>
     void msgpack_pack(Packer& pk) const
     {
         pk.pack_array(sizeof...(Args));
 
-        define_imp<std::tuple<Args&...>, sizeof...(Args)>::pack(pk, a);
+        define_array_imp<std::tuple<Args&...>, sizeof...(Args)>::pack(pk, a);
     }
     void msgpack_unpack(msgpack::object const& o)
     {
         if(o.type != msgpack::type::ARRAY) { throw msgpack::type_error(); }
 
-        define_imp<std::tuple<Args&...>, sizeof...(Args)>::unpack(o, a);
+        define_array_imp<std::tuple<Args&...>, sizeof...(Args)>::unpack(o, a);
     }
     void msgpack_object(msgpack::object* o, msgpack::zone& z) const
     {
@@ -154,15 +154,15 @@ struct define {
         o->via.array.ptr = static_cast<msgpack::object*>(z.allocate_align(sizeof(msgpack::object)*sizeof...(Args)));
         o->via.array.size = sizeof...(Args);
 
-        define_imp<std::tuple<Args&...>, sizeof...(Args)>::object(o, z, a);
+        define_array_imp<std::tuple<Args&...>, sizeof...(Args)>::object(o, z, a);
     }
 
     std::tuple<Args&...> a;
 };
 
 template <>
-struct define<> {
-    typedef define<> value_type;
+struct define_array<> {
+    typedef define_array<> value_type;
     typedef std::tuple<> tuple_type;
     template <typename Packer>
     void msgpack_pack(Packer& pk) const
@@ -181,15 +181,15 @@ struct define<> {
     }
 };
 
-inline define<> make_define()
+inline define_array<> make_define_array()
 {
-    return define<>();
+    return define_array<>();
 }
 
 template <typename... Args>
-inline define<Args...> make_define(Args&... args)
+inline define_array<Args...> make_define_array(Args&... args)
 {
-    return define<Args...>(args...);
+    return define_array<Args...>(args...);
 }
 
 }  // namespace type
@@ -198,4 +198,4 @@ inline define<Args...> make_define(Args&... args)
 /// @endcond
 }  // namespace msgpack
 
-#endif // MSGPACK_CPP11_DEFINE_HPP
+#endif // MSGPACK_CPP11_DEFINE_ARRAY_HPP
