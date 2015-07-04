6131e3d747b183b2cac9a3e95c3c6af36b2f4ce8
Takatoshi Kondo <redboltz@gmail.com>
2015-07-04 22:49:47 +0900

    Fixed double comparison in tests.

--- test/boost_fusion.cpp
+++ test/boost_fusion.cpp
@@ -1,6 +1,8 @@
 #include <msgpack.hpp>
 #include <sstream>
 #include <iterator>
+#include <cmath>
+
 #include <gtest/gtest.h>
 
 #ifdef HAVE_CONFIG_H
@@ -10,6 +12,8 @@
 #if defined(MSGPACK_USE_BOOST)
 
 #include <boost/fusion/adapted/struct/define_struct.hpp>
+
+const double kEPS = 1e-10;
 
 BOOST_FUSION_DEFINE_STRUCT(
     BOOST_PP_EMPTY(),
@@ -29,7 +33,7 @@ TEST(MSGPACK_BOOST, fusion_pack_unpack_convert)
     msgpack::unpack(ret, ss.str().data(), ss.str().size());
     mystruct val2 = ret.get().as<mystruct>();
     EXPECT_TRUE(val1.f1 == val2.f1);
-    EXPECT_TRUE(val1.f2 == val2.f2);
+    EXPECT_TRUE(fabs(val2.f2 - val1.f2) <= kEPS);
 }
 
 TEST(MSGPACK_BOOST, object_with_zone_convert)
@@ -41,7 +45,7 @@ TEST(MSGPACK_BOOST, object_with_zone_convert)
     msgpack::object obj(val1, z);
     mystruct val2 = obj.as<mystruct>();
     EXPECT_TRUE(val1.f1 == val2.f1);
-    EXPECT_TRUE(val1.f2 == val2.f2);
+    EXPECT_TRUE(fabs(val2.f2 - val1.f2) <= kEPS);
 }
 
 #endif // defined(MSGPACK_USE_BOOST)
