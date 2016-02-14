c51e8ace483b8440893c84cc7aed186a98bcb0f2
Ian Bell <ian.h.bell@gmail.com>
2016-02-13 19:19:03 -0700

    Add signed flags to MSVC integer definitions

--- include/msgpack/sysdep.h
+++ include/msgpack/sysdep.h
@@ -15,13 +15,13 @@
 #include <stdlib.h>
 #include <stddef.h>
 #if defined(_MSC_VER) && _MSC_VER < 1600
-    typedef __int8 int8_t;
+    typedef signed __int8 int8_t;
     typedef unsigned __int8 uint8_t;
-    typedef __int16 int16_t;
+    typedef signed __int16 int16_t;
     typedef unsigned __int16 uint16_t;
-    typedef __int32 int32_t;
+    typedef signed __int32 int32_t;
     typedef unsigned __int32 uint32_t;
-    typedef __int64 int64_t;
+    typedef signed __int64 int64_t;
     typedef unsigned __int64 uint64_t;
 #elif defined(_MSC_VER)  // && _MSC_VER >= 1600
 #   include <stdint.h>
