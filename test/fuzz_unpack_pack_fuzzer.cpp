b02371efe54334ea17045536162a10d83ab48968
Chris Wolfe <chriswwolfe@gmail.com>
2018-04-24 18:09:15 -0500

    no reason to have this declaration

--- test/fuzz_unpack_pack_fuzzer.cpp
+++ test/fuzz_unpack_pack_fuzzer.cpp
@@ -2,8 +2,6 @@
 
 #include "../fuzz/unpack_pack_fuzzer.cc"
 
-extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size);
-
 TEST(FUZZ_UNPACK_PACK_FUZZER, works)
 {
   EXPECT_EQ(0, LLVMFuzzerTestOneInput(0, 0));
