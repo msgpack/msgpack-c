9aae8ef72b0994ceac00f154c1fa177ada607acd
Chris Wolfe <chriswwolfe@gmail.com>
2018-05-07 20:48:35 -0500

    update command to use

--- run-test.sh
+++ run-test.sh
@@ -9,7 +9,5 @@ fi
 
 docker build -t san .
 
-echo 'Run this'
-echo 'CMAKE_CXX_COMPILER="${CXX}" CMAKE_C_COMPILER="${CC}" CMAKE_LIBRARY_PATH="${BASE}/usr/lib:${BASE}/build" GTEST_ROOT="${BASE}/usr" BOOST_ROOT="${BASE}/usr" CFLAGS="-Werror -g -fsanitize=address" CXXFLAGS="-Werror -g -fsanitize=address" ci/build_cmake.sh'
-
-docker run --rm -v $(pwd):$(pwd) -w $(pwd) -it san bash
+echo 'CTEST_OUTPUT_ON_FAILURE=1 MSGPACK_SAN="ASAN" CXX11="ON" BOOST="ON" SHARED="ON" CHAR_SIGN="unsigned" API_VERSION="3" X3_PARSE="ON" CMAKE_CXX_COMPILER="${CXX}" CMAKE_C_COMPILER="${CC}" CMAKE_LIBRARY_PATH="${BASE}/boost:${BASE}" GTEST_ROOT="${BASE}/gtest" BOOST_ROOT="${BASE}/boost" CFLAGS="-Werror -g" CXXFLAGS="-Werror -g" ci/build_cmake.sh'
+docker run --cap-add SYS_PTRACE --rm -v $(pwd):/src/msgpack-c -w /src/msgpack-c -it san bash
