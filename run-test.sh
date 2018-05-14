84aa935fe0f9b22258eb2253982d265523142728
Chris Wolfe <chriswwolfe@gmail.com>
2018-05-14 08:24:59 -0500

    base is already there

--- run-test.sh
+++ run-test.sh
@@ -9,6 +9,6 @@ fi
 
 docker build -t san .
 
-echo 'ASAN_SYMOLIZER_PATH=/usr/lib/llvm-5.0/bin/llvm-symbolizer CMAKE_CXX_COMPILER="${CXX}" CMAKE_C_COMPILER="${CC}" CMAKE_LIBRARY_PATH="${BASE}/boost:${BASE}" GTEST_ROOT="${BASE}/gtest" BOOST_ROOT="${BASE}/boost" CTEST_OUTPUT_ON_FAILURE=1 MSGPACK_SAN="ASAN" ci/build_regression.sh'
+echo 'ASAN_SYMOLIZER_PATH=/usr/lib/llvm-5.0/bin/llvm-symbolizer CMAKE_CXX_COMPILER="${CXX}" CMAKE_C_COMPILER="${CC}" CMAKE_LIBRARY_PATH="${BASE}" GTEST_ROOT="${BASE}/gtest" BOOST_ROOT="${BASE}/boost" CTEST_OUTPUT_ON_FAILURE=1 MSGPACK_SAN="ASAN" ci/build_regression.sh'
 
 docker run --cap-add SYS_PTRACE --rm -v $(pwd):/src/msgpack-c -w /src/msgpack-c -it san bash
