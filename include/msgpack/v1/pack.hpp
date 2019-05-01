33a8d8c30f11b612d3edce0d6832593aeaee6075
Takatoshi Kondo <redboltz@gmail.com>
2019-05-01 16:08:17 +0900

    Added noexcept overload for C++11 and later.

--- include/msgpack/v1/pack.hpp
+++ include/msgpack/v1/pack.hpp
@@ -629,6 +629,14 @@ private:
         m_stream.write(buf, static_cast<SizeType>(len));
     }
 
+#if !defined(MSGPACK_USE_CPP03)
+    template <typename Ret, typename Cls,  typename SizeType>
+    void append_buffer(Ret (Cls::*)(const char*, SizeType) noexcept(true), const char* buf, size_t len)
+    {
+        m_stream.write(buf, static_cast<SizeType>(len));
+    }
+#endif // !defined(MSGPACK_USE_CPP03)
+
 private:
     Stream& m_stream;
 
