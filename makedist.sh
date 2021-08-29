5ef0108088c2831383c0778a2b8665dafc842517
Takatoshi Kondo <redboltz@gmail.com>
2021-08-30 08:51:50 +0900

    Updated the version to 4.0.1
    Fixed missing file and directory in makedist.sh.

--- makedist.sh
+++ makedist.sh
@@ -19,6 +19,7 @@ tar --append --file=$filename $prefix/test
 tar --append --file=$filename $prefix/include
 tar --append --file=$filename $prefix/erb
 tar --append --file=$filename $prefix/fuzz
+tar --append --file=$filename $prefix/cmake
 
 tar --append --file=$filename $prefix/CMakeLists.txt
 tar --append --file=$filename $prefix/Files.cmake
@@ -31,7 +32,7 @@ tar --append --file=$filename $prefix/ChangeLog
 tar --append --file=$filename $prefix/NEWS
 tar --append --file=$filename $prefix/COPYING
 tar --append --file=$filename $prefix/README
-
+tar --append --file=$filename $prefix/msgpack-config.cmake.in
 rm -f $prefix
 
 gzip -f $filename
