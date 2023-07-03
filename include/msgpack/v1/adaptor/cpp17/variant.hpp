211c50c755839590c69f100cfba5ee68a0677e3c
Uy Ha <hchanuy@gmail.com>
2023-07-03 16:17:12 +0000

    enhance error handling, implement object_with_zone, and add more tests

--- include/msgpack/v1/adaptor/cpp17/variant.hpp
+++ include/msgpack/v1/adaptor/cpp17/variant.hpp
@@ -11,8 +11,6 @@
 #ifndef MSGPACK_V1_TYPE_VARIANT_HPP
 #define MSGPACK_V1_TYPE_VARIANT_HPP
 
-#define MSGPACK_USE_STD_VARIANT_ADAPTOR
-
 #if defined(MSGPACK_USE_STD_VARIANT_ADAPTOR)
 
 #include "msgpack/cpp_version.hpp"
@@ -43,20 +41,31 @@ MSGPACK_API_VERSION_NAMESPACE(v1) {
     }
   }
 
+  struct object_variant_overload {
+    object_variant_overload(msgpack::object &obj, msgpack::zone &zone)
+      : obj{obj}
+      , zone{zone} {}
+
+    template<typename T>
+    void operator()(T const &value) {
+      obj = msgpack::object(value, zone);
+    }
+
+    msgpack::object &obj;
+    msgpack::zone &zone;
+  };
   } // namespace detail
 
   template <typename... Ts>
   struct as<std::variant<Ts...>, typename std::enable_if<(msgpack::has_as<Ts>::value && ...)>::type> {
     std::variant<Ts...> operator()(msgpack::object const &o) const {
-      if (o.type != msgpack::type::ARRAY) {
+      if (  o.type != msgpack::type::ARRAY
+         || o.via.array.size != 2
+         || o.via.array.ptr[0].type != msgpack::type::POSITIVE_INTEGER
+         || o.via.array.ptr[0].via.u64 >= sizeof...(Ts)) {
         throw msgpack::type_error{};
       }
-      if (o.via.array.size != 2) {
-        throw msgpack::type_error{};
-      }
-      if (o.via.array.ptr[0].type != msgpack::type::POSITIVE_INTEGER) {
-        throw msgpack::type_error{};
-      }
+
       return detail::construct_variant<std::variant<Ts...>, Ts...>(
         o.via.array.ptr[0].as<std::size_t>(),
         o.via.array.ptr[1],
@@ -68,15 +77,13 @@ MSGPACK_API_VERSION_NAMESPACE(v1) {
   template<typename... Ts>
   struct convert<std::variant<Ts...>> {
     msgpack::object const &operator()(msgpack::object const &o, std::variant<Ts...> &v) const {
-      if (o.type != msgpack::type::ARRAY) {
+      if (  o.type != msgpack::type::ARRAY
+         || o.via.array.size != 2
+         || o.via.array.ptr[0].type != msgpack::type::POSITIVE_INTEGER
+         || o.via.array.ptr[0].via.u64 >= sizeof...(Ts)) {
         throw msgpack::type_error{};
       }
-      if (o.via.array.size != 2) {
-        throw msgpack::type_error{};
-      }
-      if (o.via.array.ptr[0].type != msgpack::type::POSITIVE_INTEGER) {
-        throw msgpack::type_error{};
-      }
+
       v =  detail::construct_variant<std::variant<Ts...>, Ts...>(
         o.via.array.ptr[0].as<std::size_t>(),
         o.via.array.ptr[1],
@@ -92,38 +99,24 @@ MSGPACK_API_VERSION_NAMESPACE(v1) {
     msgpack::packer<Stream>& operator()(msgpack::packer<Stream> &o, std::variant<Ts...> const &v) const {
       o.pack_array(2);
       o.pack_uint64(v.index());
-      std::visit([&o](auto const &real_value){o.pack(real_value);}, v);
+      std::visit([&o](auto const &value){o.pack(value);}, v);
       return o;
     }
   };
 
-  // template <typename... Ts>
-  // struct object<std::variant<Ts...>> {
-  //   void operator()(msgpack::object &o, std::variant<Ts...> const &v) const {
-  //     o.type = msgpack::type::ARRAY;
-  //     o.via.array.size = 2;
-  //     msgpack::adaptor::object<std::size_t>(o.via.array.ptr[0], v.index());
-  //     std::visit([&o](auto const &value) {
-  //       msgpack::adaptor::object(o.via.array.ptr[1], value);
-  //     }, v);
-  //   }
-  // };
-  //
-  // template<typename... Ts>
-  // struct object_with_zone<std::variant<Ts...>> {
-  //   void operator()(msgpack::object::with_zone &o, std::variant<Ts...> const &v) const {
-  //     o.type = msgpack::type::ARRAY;
-  //
-  //     msgpack::object *p = static_cast<msgpack::object *>(o.zone.allocate_align(sizeof(msgpack::object) * 2, MSGPACK_ZONE_ALIGNOF(msgpack::object)));
-  //
-  //     o.via.array.size = 2;
-  //     o.via.array.ptr = p;
-  //     msgpack::adaptor::object_with_zone<std::size_t>()(o.via.array.ptr[0], v.index(), o.zone);
-  //     std::visit([&o](auto const &real_value){
-  //       o.via.array.ptr[1] = msgpack::adaptor::object()(real_value, o.zone);
-  //     }, v);
-  //   }
-  // };
+
+  template<typename... Ts>
+  struct object_with_zone<std::variant<Ts...>> {
+    void operator()(msgpack::object::with_zone &o, std::variant<Ts...> const &v) const {
+      msgpack::object *p = static_cast<msgpack::object *>(o.zone.allocate_align(sizeof(msgpack::object) * 2, MSGPACK_ZONE_ALIGNOF(msgpack::object)));
+
+      o.type = msgpack::type::ARRAY;
+      o.via.array.size = 2;
+      o.via.array.ptr = p;
+      o.via.array.ptr[0]= msgpack::object(v.index(), o.zone);
+      std::visit(detail::object_variant_overload(o.via.array.ptr[1], o.zone), v);
+    }
+  };
   } // namespace adaptor
 }
 } // namespace msgpack
