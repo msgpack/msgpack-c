012756d6cc707df4cb823901e4b9788066671ce1
Takatoshi Kondo <redboltz@gmail.com>
2025-05-18 20:18:41 +0900

    Updated cmake minimum required to 3.5-4.0

--- cmake/CodeCoverage.cmake
+++ cmake/CodeCoverage.cmake
@@ -48,7 +48,7 @@ FUNCTION(SETUP_TARGET_FOR_COVERAGE _targetname _testrunner _outputname)
 
 	# Show info where to find the report
 	ADD_CUSTOM_COMMAND(TARGET ${_targetname} POST_BUILD
-		COMMAND ;
+        COMMAND ${CMAKE_COMMAND} -E echo ""
 		COMMENT "Open ./${_outputname}/index.html in your browser to view the coverage report."
 	)
 
